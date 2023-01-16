c0870af86275a65917aa633923bb2258f9476955
gabrielnalmeida <84048406+gabrielnalmeida@users.noreply.github.com>
2023-01-15 23:29:15 -0300

    Otimizações de Código do Alura MIDI
    Finalizado o Curso de Java Script para Web: Crie páginas dinâmicas

--- Formação_Java_Script/aluramidi/main.js
+++ Formação_Java_Script/aluramidi/main.js
@@ -1,5 +1,11 @@
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -11,8 +17,18 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    tecla.onclick = function(){
+    tecla.onclick = function (){
         tocaSom(idAudio);
     };
 
+    tecla.onkeydown = function (evento){
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add("ativa");
+        }
+    };
+    
+    tecla.onkeyup = function (){
+        tecla.classList.remove("ativa");
+    };
 }
